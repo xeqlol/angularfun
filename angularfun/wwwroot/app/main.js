940b11a61d718e5b1b352dcc4a04aeca4bcc239e
Dmitry Nemkov <xeqlol@gmail.com>
2017-09-09 20:28:50 +0500

    Add project files.

---
+++ angularfun/wwwroot/app/main.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
+platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(AppModule);
+//# sourceMappingURL=main.js.map
